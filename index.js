bab849d5a2e0639c653db52055da0fb0ef3bf422
Tulio Minini <tulio.mminini@gmail.com>
2024-03-27 21:24:55 +0000

    server up and running

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello from Express!");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
