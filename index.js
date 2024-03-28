5348bfbfa018184cd9f0f82e9e0eacdb8664b5ed
Tulio Minini <tulio.mminini@gmail.com>
2024-03-28 20:42:41 +0000

    dotenv and mongoose dependencies installed, connection with MongoDB successful

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}!`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server up and running on port ${process.env.PORT}!`);
 });
 
 app.get('/', (req, res) => {
   res.send('Hello from Node API with Nodemon!');
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected do DB!');
+  })
+  .catch(() => {
+    console.log('Connection to DB Failed!');
+  });
