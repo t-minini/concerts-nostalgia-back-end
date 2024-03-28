9492cf735460652645f46682c2b52b048eb1d3f5
Tulio Minini <tulio.mminini@gmail.com>
2024-03-28 16:11:06 +0000

    nodemon and mongoDB installed, and connection to DB to start

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}`);
+  console.log(`Server up and running on port ${PORT}!`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node API with Nodemon!');
 });
