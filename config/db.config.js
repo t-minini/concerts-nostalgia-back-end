e2a1225791d54b1202aac689403c038bb6965631
Tulio Minini <tulio.mminini@gmail.com>
2024-03-29 15:24:11 +0000

    backend connected to mongoDB database

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+  try {
+    const dbConnection = await mongoose.connect(process.env.MONGODB_URI);
+    console.log('CONNECTED TO DB:', dbConnection.connections[0].name);
+    console.log(`SERVER RUNNING ON PORT: ${process.env.PORT}`);
+  } catch (error) {
+    console.log('CONNECTION ERROR MESSAGE:', error);
+  }
+}
+
+// THIS CODE: ESTABLISHES A CONNECTION TO MONGODB DATABASE USING MONGOOSE, LOGS THE CONNECTION STATUS AND THE SERVER'S PORT NUMBER, AND HANDLES CONNECTION ERRORS.
--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
 import 'dotenv/config';
+import express from 'express';
+import { connectDB } from './config/db.config.js';
+
+connectDB();
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server up and running on port ${process.env.PORT}!`);
-});
-
 app.get('/', (req, res) => {
-  res.send('Hello from Node API with Nodemon!');
+  res.send('Hello from Node API!');
 });
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Connected do DB!');
-  })
-  .catch(() => {
-    console.log('Connection to DB Failed!');
-  });
